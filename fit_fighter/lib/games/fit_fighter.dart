c991d0150d035166dcacba07dfe901ec4d2b33e6
SHT-3756 <gkdlfndkekd@naver.com>
2023-09-21 23:42:33 +0900

    정리

--- fit_fighter/lib/games/fit_fighter.dart
+++ fit_fighter/lib/games/fit_fighter.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
 
+import 'package:fit_fighter/components/background_component.dart';
 import 'package:flame/game.dart';
 
 class FitFighterGame extends FlameGame {
   @override
   Future<void> onLoad() async {
     super.onLoad();
+    add(BackgroundComponent());
   }
 }
