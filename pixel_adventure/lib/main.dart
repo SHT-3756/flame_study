1641f6cbc74fbf3c5ed1c320cdd6f3796b16a6da
SHT-3756 <gkdlfndkekd@naver.com>
2023-09-26 00:07:16 +0900

    fix : main.dart - 풀스크린, 가로화면 비동기로 변경

--- pixel_adventure/lib/main.dart
+++ pixel_adventure/lib/main.dart
@@ -4,10 +4,11 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:pixel_adventure/pixel_adventure.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Flame.device.fullScreen(); // 풀 스크린
-  Flame.device.setLandscape(); // 가로 화면
+  // 비동기로 기다리는 이유는 처음에는 세로로 실행되니깐, 조이스틱 위치 이슈 방지 위함.
+  await Flame.device.fullScreen(); // 풀 스크린
+  await Flame.device.setLandscape(); // 가로 화면
 
   PixelAdventure game = PixelAdventure();
   runApp(
