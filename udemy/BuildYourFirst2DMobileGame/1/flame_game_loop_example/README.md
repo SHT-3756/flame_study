a75fef83b1287e0ce5745cc900d385163b83593c
SHT-3756 <gkdlfndkekd@naver.com>
2023-10-24 13:07:14 +0900

    feat : 디버그 모드로 fps 출력과, 클릭시 위치 출력 추가

--- udemy/BuildYourFirst2DMobileGame/1/flame_game_loop_example/README.md
+++ udemy/BuildYourFirst2DMobileGame/1/flame_game_loop_example/README.md
@@ -1,16 +1,5 @@
-# flame_game_loop_example
+## flame_game_loop_example
 
-A new Flutter project.
+처음 만든 플레임 게임 루프 프로젝트 예시
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## 결과
--- udemy/BuildYourFirst2DMobileGame/1/flame_game_loop_example/lib/main.dart
+++ udemy/BuildYourFirst2DMobileGame/1/flame_game_loop_example/lib/main.dart
@@ -1,16 +1,43 @@
+import 'dart:async';
+
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    GameWidget(game: MyFirstGame())
-  );
+  // 안드로이드, 아이폰 위의 기본 헤더 앱바를 제거
+  WidgetsFlutterBinding.ensureInitialized();
+  Flame.device.fullScreen();
+  runApp(GameWidget(game: MyFirstGame()));
 }
 
-class MyFirstGame extends FlameGame {
+class MyFirstGame extends FlameGame with TapDetector {
+  @override
+  bool get debugMode => true;
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  FutureOr<void> onLoad() {
+    add(FpsTextComponent(position : Vector2(10, 50)));
+    return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+  }
+
+  @override
+  void render(Canvas canvas) {
+    canvas.drawPaint(Paint()..color = Colors.red.shade200);
+    super.render(canvas);
+  }
+
+  @override
+  void onTapUp(TapUpInfo info) {
+    // 턉 위치 출력
+    print('tap location x = ${info.eventPosition.game.x}, y = ${info.eventPosition.game.y}');
+    super.onTapUp(info);
   }
 }
